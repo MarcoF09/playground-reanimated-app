8dc623135a241d798baf128707e404445d291076
Marco Fiorito <marcofiorito1@gmail.com>
2021-02-18 15:46:59 -0300

    add accordion in a specific folder

---
+++ src/scenes/AccordionScreen/Accordion/index.tsx
@@ -0,0 +1,52 @@
+import React, {useCallback} from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+import {AccordionHeader} from '../AccordionHeader';
+import {AccordionItem} from '../AccordionItem';
+import {players} from '../goatPlayers';
+import {ITEM_HEIGHT} from '../types';
+
+export const Accordion: React.FC<{}> = () => {
+  const heightAnimated = useSharedValue(0);
+
+  const onPress = useCallback(() => {
+    heightAnimated.value = withTiming(
+      heightAnimated.value === 0 ? ITEM_HEIGHT * players.length : 0,
+      {
+        duration: 400,
+      },
+    );
+  }, [heightAnimated]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    height: heightAnimated.value,
+    overflow: 'hidden',
+  }));
+
+  const animatedBorder = useAnimatedStyle(() => ({
+    borderBottomLeftRadius: heightAnimated.value === 0 ? 10 : 0,
+    borderBottomRightRadius: heightAnimated.value === 0 ? 10 : 0,
+  }));
+
+  return (
+    <>
+      <AccordionHeader
+        onPress={onPress}
+        animatedBorder={animatedBorder}
+        animatedHeight={heightAnimated}
+      />
+      <Animated.View style={animatedStyle}>
+        {players.map((goatPlayer, index) => (
+          <AccordionItem
+            {...goatPlayer}
+            isLast={index === players.length - 1}
+          />
+        ))}
+      </Animated.View>
+    </>
+  );
+};
